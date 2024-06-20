065862d264b8dea86cf456a125a5071f150a443c
Bnaya Eshet <bnayaes@payoneer.com>
2024-06-20 09:51:48 +0300

    Apache.Avro

--- Kafka-X.SchemaRegistry.SqlServerProvider/SchemaEntity.cs
+++ Kafka-X.SchemaRegistry.SqlServerProvider/SchemaEntity.cs
@@ -11,6 +11,7 @@ internal record SchemaEntity
     public required string Key { get; init; }
     public required int Version { get; init; }
     public required byte[] Definition { get; init; }
+    public DateTimeOffset? ModifiedDate { get; init; }
 
     public static implicit operator byte[] (SchemaEntity schemaEntity) => schemaEntity.Definition;
 }
---
+++ Kafka-X.SchemaRegistry.SqlServerProvider/SchemaNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿
+namespace KafkaX;
+
+[Serializable]
+internal class SchemaNotFoundException : Exception
+{
+    private SchemaNotFoundException()
+    {
+    }
+
+    private SchemaNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public SchemaNotFoundException(string key, int version): base($"Schema not found for key: {key} and version: {version}")
+    {
+    }
+
+    private SchemaNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
